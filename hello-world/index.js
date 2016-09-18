1433bca67bf3acf25f809a67411c6d915c100a39
VHero <a772839689@gmail.com>
2016-09-19 00:37:23 +0800

    忽略

--- hello-world/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-var routes=require('./routes')(app);
-app.listen(3000,function(){
-	console.log("Serve success");
-});
--- subhtml.js
+++
@@ -1,4 +0,0 @@
-function toWrapStr(str) {
-    // remove html and make it max 40 chars 
-    return str ? String(str).replace(/<[^>]+>/gm, '').substr(0, 40) : '';
-};
