aee90e45f30b3211613053b030fb520848b5c9c4
VHero <a772839689@gmail.com>
2016-09-19 00:38:36 +0800

    nodejs搭建简易电影网站

---
+++ hello-world/params.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var http=require('http');
+var app=express();
+
+app.all("*", function(request, response, next) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  next();
+});
+
+app.get("/", function(request, response) {
+  response.end("Welcome to the homepage!");
+});
+
+app.get("/about/:who?", function(req, res) {
+  res.end("Hello, " + req.params.who + ".");
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
+});
+
+http.createServer(app).listen(1337,function(){
+	console.log("Server Start")
+});
